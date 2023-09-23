4371f70aae34bbab377ac6afc123bd5918553272
androozhang <zandrew129@gmail.com>
2023-09-23 14:51:51 -0400

    Made server.js

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost/eventsdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Event = mongoose.model('Event', {
+  name: String,
+  description: String,
+  date: String,
+});
+
+app.get('/api/events', async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (error) {
+    console.error('Error fetching events:', error);
+    res.status(500).json({ error: 'An error occurred while fetching events.' });
+  }
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
