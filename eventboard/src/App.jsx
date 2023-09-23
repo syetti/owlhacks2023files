6ff89890bceb30dda5840e26dabf3f7982b8c8ed
androozhang <zandrew129@gmail.com>
2023-09-23 11:16:00 -0400

    Added component files

--- eventboard/src/App.jsx
+++ eventboard/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <div>
+      
       <Event name="Drexel Event" description="Its fun" date="9/25/23"/>
       <Event name="Temple Event" description="Its less fun" date="9/26/23"/>
       </div>
