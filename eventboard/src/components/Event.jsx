7cb9d8bb4fa14e7f9060e6111c592cd7a0ae4e06
Abdel <abdelnassam@gmail.com>
2023-09-24 01:53:40 -0400

    Changed Card CSS

--- eventboard/src/components/Event.jsx
+++ eventboard/src/components/Event.jsx
@@ -14,24 +14,28 @@ const Event = (props) => {
     };
     const containerStyle = {
         width: '100%',
-        height: '50%'
+        height: '200px'
     };
     
     return (
         <div className="Event">
             <p className="name">{props.name}</p>
             <p className='address'>{props.address} {props.city}, {props.country}</p>
-            <p className="cords"></p>
-            <LoadScript googleMapsApiKey={import.meta.env.VITE_REACT_APP_MAPS_APP_API_KEY}>
-                
-                <GoogleMap 
-                mapContainerStyle={containerStyle}
-                center = {{lat: props.latitude, lng: props.longitude}}
-                zoom = {13}
-                
-                >
-                </GoogleMap>
-            </LoadScript>
+            <div className="map-container">
+                <LoadScript googleMapsApiKey={import.meta.env.VITE_REACT_APP_MAPS_APP_API_KEY}>
+                    <GoogleMap 
+                    mapContainerStyle={containerStyle}
+                    center = {{lat: props.latitude, lng: props.longitude}}
+                    zoom = {13}
+                    
+                    >
+                        <Marker
+                        position={{ lat: props.latitude, lng: props.longitude }}
+                        />
+                    </GoogleMap>
+                </LoadScript>
+            </div>
+            
             <div className="favorite">
                 {isFavorited ? (
                 <BookmarkStarFill size={30} onClick={handleFavoriteClick} className="filled" />
