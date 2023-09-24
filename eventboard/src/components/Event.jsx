328d4aab58bb61e09dbb75dee18dd2e1a3acce4e
androozhang <zandrew129@gmail.com>
2023-09-23 22:49:39 -0400

    Added favorite button

--- eventboard/src/components/Event.jsx
+++ eventboard/src/components/Event.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import "./Event.css";
 import { Link } from "react-router-dom";
+import { BookmarkStar, BookmarkStarFill } from 'react-bootstrap-icons';
+import { useState } from "react";
 
 const Event = (props) => {
+
+    const [isFavorited, setIsFavorited] = useState(false);
+
+    const handleFavoriteClick  = () => {
+        setIsFavorited(!isFavorited);
+    };
+
     return (
         <div className="Event">
             <p className="name">{props.name}</p>
+
+            <div className="favorite">
+                {isFavorited ? (
+                <BookmarkStarFill size={30} onClick={handleFavoriteClick} className="filled" />
+                ) : (
+                <BookmarkStar size={30} onClick={handleFavoriteClick} />
+                )}
+            </div>
         </div>
     )
 
