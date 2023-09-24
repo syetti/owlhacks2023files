cbf62709c169623cba74c50dadbe6a95c006255c
Abdel <abdelnassam@gmail.com>
2023-09-24 01:27:54 -0400

    Got Maps Working

--- eventboard/src/components/Event.jsx
+++ eventboard/src/components/Event.jsx
@@ -3,6 +3,7 @@ import "./Event.css";
 import { Link } from "react-router-dom";
 import { BookmarkStar, BookmarkStarFill } from 'react-bootstrap-icons';
 import { useState } from "react";
+import{GoogleMap, useLoadScript, Marker, LoadScript} from '@react-google-maps/api';
 
 const Event = (props) => {
 
@@ -11,13 +12,26 @@ const Event = (props) => {
     const handleFavoriteClick  = () => {
         setIsFavorited(!isFavorited);
     };
-
-
+    const containerStyle = {
+        width: '100%',
+        height: '50%'
+    };
+    
     return (
         <div className="Event">
             <p className="name">{props.name}</p>
             <p className='address'>{props.address} {props.city}, {props.country}</p>
-            <p className="cords">{props.longitude} {props.latitude}</p>
+            <p className="cords"></p>
+            <LoadScript googleMapsApiKey={import.meta.env.VITE_REACT_APP_MAPS_APP_API_KEY}>
+                
+                <GoogleMap 
+                mapContainerStyle={containerStyle}
+                center = {{lat: props.latitude, lng: props.longitude}}
+                zoom = {13}
+                
+                >
+                </GoogleMap>
+            </LoadScript>
             <div className="favorite">
                 {isFavorited ? (
                 <BookmarkStarFill size={30} onClick={handleFavoriteClick} className="filled" />
--- eventboard/src/components/map.jsx
+++ eventboard/src/components/map.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, from "react";
 import{GoogleMap, useLoadScript, Marker} from '@react-google-maps/api';
 
 
@@ -7,26 +7,7 @@ const Map = ()=> {
         width: '400px',
         height: '400px'
     };
-    const {isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.MAPS_APP_API_KEY,
-    })
-    if (!isLoaded) return <div>Loading...</div>
 
-
-    const [center, setCenter] = useState({ lat: 0, lng: 0});
-
-    useEffect(() =>{ 
-        fetch('https://www.refugerestrooms.org/api/v1/restrooms/by_location')
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.latitude && data.longitude){
-                    setCenter({lat: data.latitude, lng: data.longitude});
-                }
-            })
-            .catch((error) =>{
-                console.error('Error fetching data: ', error)
-            })
-    }, []) 
 
     return(
         <LoadScript googleMapsApiKey= {process.env.MAPS_APP_API_KEY}>
