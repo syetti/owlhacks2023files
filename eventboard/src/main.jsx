8d6ebfc7b377492fed2835fe0a5e066aaa074eb2
Abdel <abdelnassam@gmail.com>
2023-09-25 21:43:22 -0400

    Cleaned up files and updated gitignore

--- eventboard/src/main.jsx
+++ eventboard/src/main.jsx
@@ -1,7 +1,6 @@
 // main.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import "bootstrap/dist/css/bootstrap.min.css";
